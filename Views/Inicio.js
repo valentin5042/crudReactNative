df0cabaab8085bdf11913de1260cafd9faf5c4e5
Valentin Urzua <urzuavales@gmail.com>
2024-04-29 15:22:13 -0600

    integracion de axios al componente de inicio

--- Views/Inicio.js
+++ Views/Inicio.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Text } from 'react-native'
+import axios from 'axios';
 
 const Inicio = () => {
+
+  // state de la app
+
+  const [ clientes, setClientes ] = useState([]);
+
+useEffect (() => {
+
+  const obtenerClientes = async () => {
+    try {
+      const resultado = await axios.get('');
+      setClientes(resultado.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  obtenerClientes();
+}, []);
+
   return (
     <>
         <Text>Desde incio</Text>
